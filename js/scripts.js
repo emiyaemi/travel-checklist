290d15fa6909bce1c37581018eef6a76dd9c5dae
Amy Hwang and Suzi Rubino <suzi@>
2016-11-08 13:16:18 -0800

    work on sending data from one html page to another

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,13 @@ Traveler.prototype.travelerList = function() {
     travelerList += "<li>" + this.travelerUser[i]+ "</li>";
   }
 }
+var travelerSentencePage = "";
 
+Traveler.prototype.travelerSentence = function () {
+  for(var i = 0; i < this.travelerUser.length; i++) {
+    travelerSentencePage += this.travelerUser[i]+ ", ";
+  }
+}
 
 // Front End
 
@@ -49,13 +55,47 @@ $(document).ready(function() {
   });
 
   //move to checklist.html page
-  $("button#to-next-page").click(function(event) {
-    event.preventDefault();
 
-    window.location.href="https://www.google.com/";
-    return false;
+$("button#to-next-page").click(function() {
 
-  
+  $("p#checkListPage").load("checklist.html"){
 
-  })
+  };
+
+});
+
+
+
+
+
+// function sendSentence() {
+//   var send = document.getElementId("to-next-page").value;
+//   url = "checklist.html/next.html?name=" + $("#checkListPage").text("test");
+//   document.location.href = url;
+// }
+//
+//
+//
+//
+//     window.location.href="checklist.html";
+//     // return false;
+//
+//     alert("on new page");
+//
+//     });
+// });
+    //NOT OUTPUTTING
+    // var who = $('#who input:checkbox:checked').val();
+    // var where = $('#where').val();
+    // var how = $('#how input:radio:checked').val();
+    // var staying = $('#staying input:radio:checked').val();
+    // var activities = $('#activities input:radio:checked').val();
+    //
+    // var newTraveler = new Traveler(where, how, staying, activities);
+
+
+    // newTraveler.travelerSentence();
+    //
+    // $("#checkListPage").text(travelerSentencePage + "traveling to " + newTraveler.travelPlace +" by "+ newTraveler.travelMode +  ". Accommodation: " + newTraveler.travelAccom + ". Planned activity: " + newTraveler.travelAct);
+
 });
